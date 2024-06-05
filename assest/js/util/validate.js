d2e779698d7442c0861253e9b897d97bd80e4775
Long thu ba <longmentor55@gmail.com>
2024-06-05 09:25:53 +0700

    fix: update features bug empty label

--- assest/js/util/validate.js
+++ assest/js/util/validate.js
@@ -1,6 +1,6 @@
 const checkLength = (label) => {
     if (label.length > 10 || label.length === 0) {
-        alert('So long label or empty label');
+        alert('So long label or empty label, must be at least 10 characters long');
         return false;
     }
     return true;
